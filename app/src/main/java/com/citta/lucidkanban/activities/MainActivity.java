2ac3084e965641659f10764f6cbc7fc83d110da4
Satya <satya@eatsleepride.com>
2019-06-02 15:33:44 -0400

    added storage

--- app/src/main/java/com/citta/lucidkanban/activities/MainActivity.java
+++ app/src/main/java/com/citta/lucidkanban/activities/MainActivity.java
@@ -3,9 +3,6 @@ package com.citta.lucidkanban.activities;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,17 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
-
-import java.util.List;
-
-import io.github.luizgrp.sectionedrecyclerviewadapter.SectionParameters;
-import io.github.luizgrp.sectionedrecyclerviewadapter.SectionedRecyclerViewAdapter;
-import io.github.luizgrp.sectionedrecyclerviewadapter.StatelessSection;
 
 import com.citta.lucidkanban.fragments.TasksFragment;
-import com.citta.lucidkanban.model.DummyDataProvider;
-import com.citta.lucidkanban.model.Task;
 import com.citta.lucidkanban.R;
 
 public class MainActivity extends AppCompatActivity
@@ -58,7 +46,6 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-//        initializeRecyclerView();
     }
 
 
---
+++ app/src/main/java/com/citta/lucidkanban/data/Storage.java
@@ -0,0 +1,76 @@
+package com.citta.lucidkanban.data;
+
+import android.content.Context;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * Package: com.citta.lucidkanban.data
+ * User: Satya
+ * Date: 2019-06-02
+ * Time: 10:14
+ */
+public class Storage {
+
+    private Context context;
+
+    public Storage(Context context) {
+        this.context =context;
+    }
+
+    public void store(Object object, String fileName, Directory directoryName) {
+        try {
+            File directory = context.getDir(directoryName.getPathName(), Context.MODE_PRIVATE);
+            File file = new File(directory, fileName);
+
+            FileOutputStream fileOut =
+                    new FileOutputStream(file);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(object);
+            out.close();
+            fileOut.close();
+            System.out.println("Serialized data is saved in "+file.getPath());
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+
+    public Object retrieve(String fileName, Directory directoryName) {
+        Object localObject = null;
+
+        try {
+            File directory = context.getDir(directoryName.getPathName(), Context.MODE_PRIVATE);
+            File file = new File(directory, fileName);
+            FileInputStream fileIn = new FileInputStream(file);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            localObject = in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+        } catch (ClassNotFoundException c) {
+            System.out.println(" class not found");
+            c.printStackTrace();
+        }
+        return localObject;
+    }
+
+    public enum Directory{
+        Documents,
+        Cache;
+
+        public String getPathName() {
+            switch (this){
+                case Documents: return "documents";
+                case Cache: return "cache";
+                default: return "documents";
+            }
+        }
+    }
+
+}
